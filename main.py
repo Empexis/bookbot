4e4da843ad8914efbc8f741a911d34195a3c1ebe
Empexis <mfritz70@gmail.com>
2025-07-19 11:44:28 -0500

    created function to get text

---
+++ main.py
@@ -0,0 +1,12 @@
+#
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_path = 'books/frankenstein.txt'
+    book_text = get_book_text(file_path)
+    print(book_text)
+
+main()

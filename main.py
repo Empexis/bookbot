98681bcdbc2b1ab6e8e4789e7abc5d7f732965d9
Empexis <mfritz70@gmail.com>
2025-07-19 20:11:52 -0500

    finished!

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#imports
+import sys
+from stats import word_count
+from stats import char_count
+from stats import sort_char_dict
+
 #functional code for bookbot
 def get_book_text(path):
     with open(path) as f:
@@ -5,26 +11,25 @@ def get_book_text(path):
     return contents
 
 def main():
-    path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2 :
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     book_text = get_book_text(path)
-    return(book_text)
-
-#imports from stats.py
-from stats import word_count
-from stats import char_count
-from stats import sort_char_dict
+    return(path, book_text)
 
 #the output
-def report():
+def report(path, book_text):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
-    book_data = main()
-    print(f"Found {word_count(book_data)} total words.")
+    print(f"Found {word_count(book_text)} total words.")
     print("---------- Character Count -------")
-    for char in sort_char_dict(char_count(book_data)):
+    for char in sort_char_dict(char_count(book_text)):
         if char['char'].isalpha():
             print(f"{char['char']}: {char['num']}")
     print("============= END ==============")
 
-report()
+
+path, book_text = main()
+report(path, book_text)
